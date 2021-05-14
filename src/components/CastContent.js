75bac74a6d0bc4340bc6207421f458ae25cc5627
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-05-14 18:45:22 +0530

    title

--- src/components/CastContent.js
+++ src/components/CastContent.js
@@ -9,9 +9,125 @@ const CastContent = () => {
                     <HeaderA href="">Cast</HeaderA>
                 </HeaderH>
             </HeaderH1>
+            <Div1>
+                <DivTile>
+                    <UlTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                        <LiTile>
+                            <ATile href="cucek.in">
+                                <ImgContainer>
+                                    <ImgTag src="https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2016/10/vikings_season4_cast_ragnar_16_9.jpg?w=548" alt />
+                                </ImgContainer>
+                                <DetailsDiv>
+                                    <DetailsStrong>Ragnar</DetailsStrong>
+                                    <DetailsSmall>Played by Travis Fimmel</DetailsSmall>
+                                </DetailsDiv>
+                            </ATile>
+                        </LiTile>
+                    </UlTile>
+                </DivTile>
+            </Div1>
         </ContentDiv>
     )
-} 
+}
 
 const ContentDiv = styled.div`
     text-align: center;
@@ -43,6 +159,124 @@ const HeaderH = styled.h1`
 const HeaderA = styled.a`
     text-decoration: none;
     color: black;
+`;
+
+const Div1 = styled.div`
+    @media (min-width: 961px) {
+    width: 100%;
+    display: block;
+    margin: 0 auto;
+    display: inline-block;
+    vertical-align: top;
+    width: calc(100% - 300px - 16px - 5px);
+    }
+`;
+
+const DivTile = styled.div`
+    -webkit-font-smoothing: antialiased;
+    @media (min-width: 961px) {
+    margin-bottom: 32px;
+    }
+    @media (min-width: 768px) and (max-width: 1024px) {
+    font-size: 14px;
+    }
+    @media (max-width: 767px) {
+    font-size: 14px;
+    margin-bottom: 16px;
+    }
+`;
+
+const UlTile = styled.ul`
+    @media (min-width: 961px) {
+    margin-left: -16px;
+    }
+    @media (max-width: 767px) {
+    margin-left: -8px;
+    }
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+`;
+
+const LiTile = styled.li`
+    border-radius: 2px;
+    display: inline-block;
+    vertical-align: top;
+    @media (min-width: 1025px) {
+        transition: all 0.3s ease-in-out;
+        &:hover{
+            box-shadow: 0 4px 16px rgb(0 0 0 / 40%);
+        }
+    }
+    @media (min-width: 768px) and (max-width: 1024px) {
+    width: calc(33.333% - 16px);
+    margin-left: 16px;
+    margin-bottom: 16px; 
+    }
+    @media (max-width: 767px) {
+    width: calc(50% - 8px);
+    margin-left: 8px;
+    margin-bottom: 8px;
+    }
+    @media (min-width: 961px) {
+    width: calc(25% - 16px);
+    margin-left: 16px;
+    margin-bottom: 16px;
+    }
+`;
+
+const ATile = styled.a`
+    display: block;
+    text-decoration: none;
+`;
+
+const ImgContainer = styled.div`
+    padding-bottom: 60%;
+    display: block;
+    position: relative;
+    color: #fff;
+    overflow: hidden;
+    height: 0;
+`;
+
+const ImgTag = styled.img`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    min-height: 100%;
+    min-width: 100%;
+    max-height: 100%;
+    width: auto;
+    transform: translate(-50%, -50%);
+`;
+
+const DetailsDiv = styled.div`
+    background: #f8f8f8;
+    font-size: 1em;
+    display: block;
+    @media (min-width: 961px) {
+    height: 100px;
+    padding: 6%;
+    }
+    @media (max-width: 767px) {
+    height: 86px;
+    padding: 4%;
+    }
+`;
+
+const DetailsStrong = styled.strong`
+    line-height: 1.4em;
+    color: black;
+    display: block;
+`;
+
+const DetailsSmall = styled.small`
+    display: block;
+    font-size: 0.75em;
+    color: rgba(0, 0, 0, 0.5);
+    font-weight: 600;
+    line-height: 1.2em;
+    margin-top: 4px;
 `;
 
 export default CastContent
